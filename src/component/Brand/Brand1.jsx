90d8a6c89ef9182f1d1461590362d84d5d2bd0f9
Arpit Agarwal <aarpit688@gmail.com>
2025-11-06 12:04:24 +0530

    ScrollToTop

--- src/component/Brand/Brand1.jsx
+++ src/component/Brand/Brand1.jsx
@@ -2,19 +2,25 @@ import React from "react";
 import Hero_brand from "../Brands/Hero_brand.jsx";
 import Story_brand from "../Brands/Story_brand.jsx";
 import WebsiteBrand2 from "../Brands/website_brand_2.jsx";
-import { useTheme } from '../../contexts/ThemeContext';
-import Scrolltotop from "../Scrolltotop.jsx";
+import { useTheme } from "../../contexts/ThemeContext";
+import ScrollToTop from "../ScrollToTop.jsx";
 
 // Updated import to use lowercase filename
 export default function Brand1() {
   const { isDarkMode } = useTheme();
-  
+
   return (
-    <div className={isDarkMode ? 'dark bg-gray-900 text-white' : 'light bg-gray-50 text-gray-900'}>
-     <Scrolltotop/>
-     <Hero_brand />
-     <Story_brand />
-     <WebsiteBrand2 />
+    <div
+      className={
+        isDarkMode
+          ? "dark bg-gray-900 text-white"
+          : "light bg-gray-50 text-gray-900"
+      }
+    >
+      <ScrollToTop />
+      <Hero_brand />
+      <Story_brand />
+      <WebsiteBrand2 />
     </div>
   );
 }
--- src/component/Brand/Brand2.jsx
+++ src/component/Brand/Brand2.jsx
@@ -2,19 +2,25 @@ import React from "react";
 import Hero_brand_2 from "../Brands/Hero_brand_2";
 import Story_brand_2 from "../Brands/Story_brand_2";
 import WebsiteBrand3 from "../Brands/website_brand_3.jsx";
-import { useTheme } from '../../contexts/ThemeContext';
-import Scrolltotop from "../Scrolltotop.jsx";
+import { useTheme } from "../../contexts/ThemeContext";
+import ScrollToTop from "../ScrollToTop.jsx";
 
 // Force rebuild by adding comment
 export default function Brand2() {
   const { isDarkMode } = useTheme();
-  
+
   return (
-    <div className={isDarkMode ? 'dark bg-gray-900 text-white' : 'light bg-gray-50 text-gray-900'}>
-        <Scrolltotop/>
-     <Hero_brand_2 />
-     <Story_brand_2 />
-     <WebsiteBrand3 />
+    <div
+      className={
+        isDarkMode
+          ? "dark bg-gray-900 text-white"
+          : "light bg-gray-50 text-gray-900"
+      }
+    >
+      <ScrollToTop />
+      <Hero_brand_2 />
+      <Story_brand_2 />
+      <WebsiteBrand3 />
     </div>
   );
 }
